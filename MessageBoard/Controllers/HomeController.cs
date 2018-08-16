b61c8104e3374adaec21515d0b6048f1d75e3d83
Po-CHEN LEE <bob.lee@ehsn.com.tw>
2018-08-16 13:39:39 +0800

    Init Database, DB tool

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MessageBoard.Models;
+using MessageBoard.Tools;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBTool dBTool = new DBTool();
+        string userConnection = ConfigurationManager.ConnectionStrings["UserConnection"].ConnectionString;
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
