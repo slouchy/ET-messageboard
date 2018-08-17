0fb1614ff4578d6909b8f04b7b5dec521b594360
Po-CHEN LEE <bob.lee@ehsn.com.tw>
2018-08-17 16:41:30 +0800

    增加使用者註冊、忘記密碼的頁面連結

---
+++ MessageBoard/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
